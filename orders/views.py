b4d3290e65e342546a9aec8f5da5cdf7b26e4294
Tanja Rietveldt <diafox2015@gmail.com>
2019-03-22 18:51:57 +0100

    add view.py OrderList and template

--- orders/views.py
+++ orders/views.py
@@ -11,7 +11,6 @@ class CreateOrder(View):
     Display existing order or
     create a new one triggered by cart status => accepted False
     """
-    # LoginRequiredMixin????
     def post(self,request):
         pk_cart = request.POST.get('pk','pk not found')
         cart = Cart.objects.get(id=pk_cart,accepted=False)
@@ -23,19 +22,10 @@ class CreateOrder(View):
         request.session["cart_id"] = new_cart.id
         return redirect('orders:list-orders')
 
-
-#
-#     def post(self,request):
-#         cart = get_object_or_404(Cart,id=request.POST.get('pk'))
-#         total = cart.cart_items.aggregate(total=Sum('sub_total'))
-#         summa = total.get('total')
-        # here += shipping_total
-        # let op *1001 ...order = Order.objects.create(cart=cart,accepted=False,total=summa) # per default accepted=False)
-        # cart.accepted = True
-        # cart.save()
-        # new_cart = Cart.objects.create(user=request.user)
-        #return render(request,'orders/create_order.html',{'cart':cart,'summa':summa,'order':order})
-#
 class ListOrder(ListView):
     model = Order
+    context_object_name = "orders"
     template_name = 'orders/list_orders.html'
+
+    def get_queryset(self):
+        return  Order.objects.filter(accepted=False)
